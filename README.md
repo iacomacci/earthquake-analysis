cb1ad35f29bf4c64a554e709bb1dd5410750a059
Walter iacomacci <walteriacomacci@hotmail.com>
2025-05-07 17:51:31 -0400

    Polished a little bit more the analysis.py and added a description to the readme.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# earthquake-analysis
+# 🌍 Earthquake Data Dashboard with Folium
 
+## 📌 Project Overview
+This project fetches global earthquake data from the USGS API, cleans it, visualizes it using Folium, and provides insights into earthquake frequency and magnitude across time and geography.
+
+## 🔍 Features
+- Dynamic magnitude-based color map (yellow → red)
+- Interactive map with popups
+- Summary statistics
+- Modular, reusable Python scripts
+
+## 🛠️ How to Run
+```bash
+python visualize_earthquakes.py #If you want to visualize the data.
+python analysis.py #If you want to check the insights and summary of the data.
--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
+import pandas as pd
+from datetime import datetime
 from fetch_earthquakes import get_earthquake_data
-
 
 df, yearly, monthly = get_earthquake_data()
 
@@ -10,10 +11,15 @@ print('Average Magnitude:', round(df['magnitude'].mean(),2))
 print('Minimum Depth:', df['depth_km'].min(), 'km')
 print('Maximum Depth:', df['depth_km'].max(), 'km')
 
+most_common_location = df['location'].mode()[0]
+count_most_common_location = df['location'].value_counts()[most_common_location]
+
+print(f"Most common location: {most_common_location} (appears {count_most_common_location} times)")
+
+
 
 print('____________________________________________________________________________________________________________________________')
-print('Key information: \n ', 
-      'Positive depth values indicate how far below the surface the earthquake occurred.')
-
-print('A negative depth might suggest a surface-level event on high ground, like a mountain or volcano. \n',
+print('Key information:\n', 
+      'Positive depth values indicate how far below the surface the earthquake occurred.\n',
+      'A negative depth might suggest a surface-level event on high ground, like a mountain or volcano. \n',
       'But it is more often a data artifact or modeling inaccuracy—USGS and other agencies sometimes revise those values later')
